abe48a2574b43c97cfa793a1dc1e5dd45a85188e
Alisa <a.arthopkins@gmail.com>
2022-03-06 19:40:20 +0200

    The main loop

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    running = True
+    while running:
+        # Grabs events such as key pressed, mouse pressed and so.
+        # Going through all the events that happened in the last clock tick
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
